b7b81514e4b1eabb8a686e4a6a23dde1f8622e3b
reginaldo-marinho <reginaldomarinho2016@gmail.com>
2022-02-01 21:22:34 -0300

    2 files modified

---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  template: `<h2>trabalhando com formulario Reativo</h2>`,
+})
+export class AppComponent {}
