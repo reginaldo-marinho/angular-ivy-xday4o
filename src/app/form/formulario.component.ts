a4e07d1cf243c874bd2c42607c1f893baf90bf18
reginaldo-marinho <reginaldomarinho2016@gmail.com>
2022-02-01 21:43:30 -0300

    2 files modified, 3 added, 3 deleted

---
+++ src/app/form/formulario.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent {}
