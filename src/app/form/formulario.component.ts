ec1e83fd663b1a0be60afaa71aa8793d91dc2774
reginaldo-marinho <reginaldomarinho2016@gmail.com>
2022-02-01 21:44:32 -0300

    1 file modified

--- src/app/form/formulario.component.ts
+++ src/app/form/formulario.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent {}
